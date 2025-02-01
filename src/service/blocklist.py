f567ce2b5aebcad86c5be3c6a55156d58d282de9
yiranlii <liyiran728@gmail.com>
2025-02-01 15:19:01 -0500

    coded initial add and delete functionalities

--- src/service/blocklist.py
+++ src/service/blocklist.py
@@ -2,7 +2,7 @@
 # -*- encoding=utf8 -*-
 
 from src.config import Config
-from src.api import ListBlockListResponse, BlockListType, ResponseStatus, BlockListResponse
+from src.api import ListBlockListResponse, BlockListType, ResponseStatus, BlockListResponse, EditBlockListResponse
 from src.db import MongoDB
 
 
@@ -24,3 +24,45 @@ class BlockListService(object):
         ]
 
         return ListBlockListResponse(blocklist=blocklist, status=ResponseStatus.SUCCESS)
+
+    def add_blocklist(self, user_id: str, website_url: str, list_type: BlockListType) -> EditBlockListResponse:
+        """Add an url to blocklist."""
+        collection = self.db.get_collection("blocklist")
+        new_entry = {
+            "user_id": user_id,
+            "website_url": website_url,
+            "list_type": list_type
+        }
+        # Do we need to save this as a result?
+        collection.insert_one(new_entry)
+
+        return EditBlockListResponse(
+            status=ResponseStatus.SUCCESS,
+            user_id=user_id,
+            website_url=website_url,
+            list_type=list_type
+        )
+    
+    def delete_blocklist(self, user_id: str, website_url: str, list_type: BlockListType) -> EditBlockListResponse:
+        """Delete an url from blocklist."""
+        collection = self.db.get_collection("blocklist")
+        query = {
+            "user_id": user_id,
+            "domain": website_url,
+            "list_type": list_type
+        }
+        result = collection.delete_one(query)
+        # if nothing was deleted, return a failed response
+        if result.deleted_count == 0:
+            return EditBlockListResponse(
+                status=ResponseStatus.FAILED,
+                user_id=user_id,
+                website_url=website_url,
+                list_type=list_type
+            )
+        return EditBlockListResponse(
+            status=ResponseStatus.SUCCESS,
+            user_id=user_id,
+            website_url=website_url,
+            list_type=list_type
+        )
