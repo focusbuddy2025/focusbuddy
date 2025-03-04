60969725c56b17dc1ec0627a2bb883366b8aec95
segunfUWaterloo <sfagbami@uwaterloo.ca>
2025-03-04 01:43:59 -0500

    refactor daily and weekly function to query focus_timer table directly

--- src/service/analytics.py
+++ src/service/analytics.py
@@ -2,6 +2,7 @@
 # -*- encoding=utf8 -*-
 
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 from src.api import (
     AnalyticsListResponse,
@@ -24,60 +25,163 @@ class AnalyticsListService(object):
         self.cfg = cfg
         self.db = MongoDB().db
 
+    def _get_daily_focus_total(self, user_id: str) -> float:
+        """Get daily summary per user"""
+        collection = self.db.get_collection("focus_timer")
+        start_date_filter = datetime.now(ZoneInfo("America/Toronto"))
+        end_date_filter = start_date_filter + timedelta(days=1)
+
+        start_date_str = start_date_filter.strftime("%m/%d/%Y")
+        end_date_str = end_date_filter.strftime("%m/%d/%Y")
+
+        pipeline = [
+            {
+                "$match": {
+                    "start_date": {
+                        "$gte": start_date_str,
+                        "$lt": end_date_str,
+                    },
+                    "session_status": 3,
+                    "user_id": user_id,
+                }
+            },
+            {
+                "$group": {
+                    "_id": None,
+                    "total_duration": {"$sum": "$duration"},
+                }
+            },
+        ]
+
+        daily_focus_total = list(collection.aggregate(pipeline))
+
+        if daily_focus_total:
+            return _convert_to_hours(daily_focus_total[0]["total_duration"])
+        else:
+            return 0
+
+    def _get_weekly_focus_total(self, user_id: str) -> float:
+        """Get weekly summary per user"""
+        collection = self.db.get_collection("focus_timer")
+        dow = datetime.now(ZoneInfo("America/Toronto")).isoweekday()
+        start_date_filter = datetime.now(ZoneInfo("America/Toronto")) - timedelta(
+            days=(dow)
+        )
+        if dow == 7:
+            start_date_filter = datetime.now(ZoneInfo("America/Toronto"))
+        end_date_filter = start_date_filter + timedelta(days=7)
+
+        start_date_str = start_date_filter.strftime("%m/%d/%Y")
+        end_date_str = end_date_filter.strftime("%m/%d/%Y")
+
+        pipeline = [
+            {
+                "$match": {
+                    "start_date": {
+                        "$gte": start_date_str,
+                        "$lt": end_date_str,
+                    },
+                    "session_status": 3,
+                    "user_id": user_id,
+                }
+            },
+            {
+                "$group": {
+                    "_id": None,
+                    "total_duration": {"$sum": "$duration"},
+                }
+            },
+        ]
+
+        weekly_focus_total = list(collection.aggregate(pipeline))
+
+        if weekly_focus_total:
+            return _convert_to_hours(weekly_focus_total[0]["total_duration"])
+        else:
+            return 0
+
+    def _all_completed_sessions(self, user_id: str) -> int:
+        """Get count of all completed sessions (session_status 3) for a user"""
+        collection = self.db.get_collection("focus_timer")
+
+        pipeline = [
+            {"$match": {"user_id": user_id, "session_status": 3}},
+            {"$count": "total"},
+        ]
+
+        total_completed_sessions = list(collection.aggregate(pipeline))
+
+        if total_completed_sessions:
+            results = total_completed_sessions[0]["total"]
+        else:
+            results = 0
+
+        return results
+
     def get_analytics(self, user_id: str) -> AnalyticsListResponse:
         """Get analytics per user."""
-        collection = self.db.get_collection("analytics")
-        query = {"user_id": user_id}
-        analytics_result: dict = collection.find_one(query)
-        if analytics_result is None:
-            return AnalyticsListResponse(
-                daily=0, weekly=0, completed_sessions=0, status=ResponseStatus.FAILED
-            )
+        daily_total = self._get_daily_focus_total(user_id)
+        weekly_total = self._get_weekly_focus_total(user_id)
+        completed_sessions_total = self._all_completed_sessions(user_id)
 
         return AnalyticsListResponse(
-            daily=_convert_to_hours(analytics_result["daily"]),
-            weekly=_convert_to_hours(analytics_result["weekly"]),
-            completed_sessions=analytics_result["completed_sessions"],
+            daily=daily_total,
+            weekly=weekly_total,
+            completed_sessions=completed_sessions_total,
             status=ResponseStatus.SUCCESS,
         )
+
+    # def get_analytics(self, user_id: str) -> AnalyticsListResponse:
+    #     """Get analytics per user."""
+    #     collection = self.db.get_collection("analytics")
+    #     query = {"user_id": user_id}
+    #     analytics_result: dict = collection.find_one(query)
+    #     if analytics_result is None:
+    #         return AnalyticsListResponse(
+    #             daily=0, weekly=0, completed_sessions=0, status=ResponseStatus.FAILED
+    #         )
+
+    #     return AnalyticsListResponse(
+    #         daily=_convert_to_hours(analytics_result["daily"]),
+    #         weekly=_convert_to_hours(analytics_result["weekly"]),
+    #         completed_sessions=analytics_result["completed_sessions"],
+    #         status=ResponseStatus.SUCCESS,
+    #     )
 
     def get_weekly_analytics_per_session_type(
         self, user_id: str
     ) -> list[AnalyticsWeeklySummaryResponse]:
         """Get weekly summary per user per session type"""
         collection = self.db.get_collection("focus_timer")
-        dow = datetime.now().isoweekday()
-        start_date_filter = datetime.now() - timedelta(days=(dow))
+        dow = datetime.now(ZoneInfo("America/Toronto")).isoweekday()
+        start_date_filter = datetime.now(ZoneInfo("America/Toronto")) - timedelta(
+            days=(dow)
+        )
         if dow == 7:
-            start_date_filter = datetime.now()
+            start_date_filter = datetime.now(ZoneInfo("America/Toronto"))
         end_date_filter = start_date_filter + timedelta(days=7)
-        pipeline = []
 
-        pipeline.append(
+        start_date_str = start_date_filter.strftime("%m/%d/%Y")
+        end_date_str = end_date_filter.strftime("%m/%d/%Y")
+
+        pipeline = [
             {
                 "$match": {
-                    "$and": [
-                        {
-                            "start_date": {
-                                "$gte": start_date_filter.strftime("%Y-%m-%d"),
-                                "$lt": end_date_filter.strftime("%Y-%m-%d"),
-                            }
-                        },
-                        {"session_status": {"$eq": 3}},
-                        {"user_id": {"$eq": user_id}},
-                    ]
+                    "start_date": {
+                        "$gte": start_date_str,
+                        "$lt": end_date_str,
+                    },
+                    "session_status": {"$eq": 3},
+                    "user_id": {"$eq": user_id},
                 }
-            }
-        )
-
-        pipeline.append(
+            },
             {
                 "$group": {
                     "_id": {"session_type": "$session_type", "user_id": "$user_id"},
                     "duration": {"$sum": "$duration"},
                 }
-            }
-        )
+            },
+        ]
 
         pipeline.append({"$set": {"user_id": "$_id"}})
 
